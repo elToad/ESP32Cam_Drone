d227faa5954a100ee8d269c21fe94c8ddd115f73
elToad <tobiasnl@hotmail.no>
2026-01-14 21:30:02 +0100

    init

--- src/IMU.hpp
+++ src/IMU.hpp
@@ -4,6 +4,8 @@
 
 class IMU : Adafruit_ICM20948{
     private:
+    int SDA, SCL, Clock;
+    double AX,AY,AZ,MX,MY,MZ;
     double RawRoll,RawPitch,RawYaw;
     double GyroXOffset,GyroYOffset,GyroZOffset;
     Adafruit_ICM20948 icm;
@@ -11,13 +13,12 @@ class IMU : Adafruit_ICM20948{
 
     public:
     double GX,GY,GZ;
-    IMU();
-    void begin();
+    IMU(int SDA,int SCL, int clock);
+    ~IMU();
     void Update();
     double GetRoll();
     double GetPitch();
     double GetYaw();
     void CalibrateGyro();
-    void CalibrateMagnoHardIron();
-    // void CalibrateMagnoSoftIron();
+    void CalibrateMagno();
 };
